d98b5c61967a817094a8446e6a1fcc07bd67c4d2
Kalam Pinjar <owaiskal57@gmail.com>
2024-06-01 22:50:01 +0530

    made some changes to UI

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ app.get("/surah/:id", async (req, res) => {
   });
 });
 
+app.get("/quran/:id", async (req, res) => {
+  const id = req.params.id;
+  const Surahverses = await surahData(id);
+  const surahList = await surahListNames();
+  const audioId = await surahList[id - 1];
+  const audio = await audioData(audioId.name);
+  res.render(join(__dirname, "views", "surah.ejs"), {
+    surah: Surahverses,
+    audio: audio,
+    surahList: surahList,
+  });
+})
+
+
 app.get("/quran", async (req, res) => {
   const data = await metaData();
   const otherData = await otherMetaData();
@@ -46,7 +60,7 @@ app.get("/quran", async (req, res) => {
   });
 });
 
-app.get("/hadith", async (req, res) => {
+app.get("/hadith/books", async (req, res) => {
   const data = await hadithBooks();
   res.render(join(__dirname, "views", "hadithbooks.ejs"), {
     books: data,
@@ -55,23 +69,32 @@ app.get("/hadith", async (req, res) => {
 
 app.get("/hadith/books/:name", async (req, res) => {
   const name = req.params.name;
+  const page = req.params.page;
   const chapters = await hadithChapters(name);
+  const url = `https://www.hadithapi.com/api/hadiths?page=${page}`;
+  const navigate = await hadithData(url);
+  const hadiths = await navigate;
   res.render(join(__dirname, "views", "hadithchapters.ejs"), {
     chapters: chapters,
     name: name,
+    hadiths: hadiths,
+    page: page,
   });
 });
 
-app.get("/hadith/books/:name/chapters/:chapter", async (req, res) => {
-  const name = req.params.name;
-  const chapter = req.params.chapter;
-  const hadiths = await hadithData();
-  res.render(join(__dirname, "views", "hadith.ejs"), {
-    hadiths: hadiths,
-    name: name,
-    chapter: chapter,
-  });
-});
+app.get(
+  "/hadith/page/:page",
+  async (req, res) => {
+    const page = req.params.page;
+    const url = `https://www.hadithapi.com/api/hadiths?page=${page}`;
+    const navigate = await hadithData(url);
+    const hadiths = await navigate;
+    res.render(join(__dirname, "views", "hadith.ejs"), {
+      hadiths: hadiths,
+      page: page,
+    });
+  }
+);
 
 const port = 5500;
 
